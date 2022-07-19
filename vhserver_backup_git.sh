1d24348edfb7c978d71dbed2b50025928cae87de
vhserver <medaluka@gmail.com>
2022-07-19 15:01:10 +0300

    Adding backup Hellheim-2022-07-19-15.00.tar.gz, keeping last 7 backups and deleting the rest

--- vhserver_backup_git.sh
+++ vhserver_backup_git.sh
@@ -3,7 +3,7 @@ DateTime=`date +%F-%H.%M`
 BackupCount=7
 RemoveCount=`expr $BackupCount + 1`
 vhserver stop
-tar -czvf /home/vhserver/valheim_Hellheim_server/backup/Hellheim-${DateTime}.tar.gz /home/vhserver/.config/unity3d/IronGate/Valheim/worlds/*
+tar -czvf /home/vhserver/valheim_Hellheim_server/backup/Hellheim-${DateTime}.tar.gz /home/vhserver/.config/unity3d/IronGate/Valheim/worlds_local/Hellheim*
 cd /home/vhserver/valheim_Hellheim_server/backup/
 ls -tp | grep -v '/$' | tail -n +$RemoveCount | xargs -I {} rm -- {}
 cd /home/vhserver/valheim_Hellheim_server
