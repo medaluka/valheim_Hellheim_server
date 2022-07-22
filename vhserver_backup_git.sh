9e7ec3de57e7fa9b7cba4e2f4e84e9a7ce2782df
vhserver <medaluka@gmail.com>
2022-07-22 19:51:21 +0300

    Adding backup Hellheim-2022-07-22-19.51.tar.gz, keeping last 2 backups and deleting the rest

--- vhserver_backup_git.sh
+++ vhserver_backup_git.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 DateTime=`date +%F-%H.%M`
-BackupCount=7
+BackupCount=2
 RemoveCount=`expr $BackupCount + 1`
+world_dir=/home/vhserver/.config/unity3d/IronGate/Valheim/worlds_local
+backup_dir=/home/vhserver/valheim_Hellheim_server/backup
+
 vhserver stop
-tar -czvf /home/vhserver/valheim_Hellheim_server/backup/Hellheim-${DateTime}.tar.gz /home/vhserver/.config/unity3d/IronGate/Valheim/worlds_local/Hellheim*
+
+cd $world_dir
+tar -czvf ${backup_dir}/Hellheim-${DateTime}.tar.gz Hellheim.db Hellheim.fwl
+
 cd /home/vhserver/valheim_Hellheim_server/backup/
 ls -tp | grep -v '/$' | tail -n +$RemoveCount | xargs -I {} rm -- {}
+
 cd /home/vhserver/valheim_Hellheim_server
 git add .
 git commit -a -m "Adding backup Hellheim-${DateTime}.tar.gz, keeping last $BackupCount backups and deleting the rest"
 git push
+
 vhserver start
+
