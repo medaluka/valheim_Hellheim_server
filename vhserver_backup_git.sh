8ee6b2025d47e09b3ab09e6b24204773284d02ba
vhserver <medaluka@gmail.com>
2022-07-24 12:56:30 +0300

    Adding backup Hellheim.tar.gz 2022-07-24-12.56, keeping last 5 backups and deleting the rest

--- vhserver_backup_git.sh
+++ vhserver_backup_git.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 DateTime=`date +%F-%H.%M`
-BackupCount=2
+BackupCount=5
 RemoveCount=`expr $BackupCount + 1`
 world_dir=/home/vhserver/.config/unity3d/IronGate/Valheim/worlds_local
-backup_dir=/home/vhserver/valheim_Hellheim_server/backup
+backup_dir=/home/vhserver/valheim_Hellheim_server/backup_latest
+backup_old_dir=/home/vhserver/valheim_Hellheim_server/backup_old
 
+
+echo "##### Stopping vhserver #####"
 vhserver stop
 
-cd $world_dir
-tar -czvf ${backup_dir}/Hellheim-${DateTime}.tar.gz Hellheim.db Hellheim.fwl
+echo "##### Moving previous backup #####"
+mv ${backup_dir}/Hellheim.tar.gz ${backup_old_dir}/Hellheim-${DateTime}.tar.gz
 
-cd /home/vhserver/valheim_Hellheim_server/backup/
+
+echo "##### Removing old backups backup #####"
+cd ${backup_old_dir}
 ls -tp | grep -v '/$' | tail -n +$RemoveCount | xargs -I {} rm -- {}
 
+echo "##### Creating new backup #####"
+cd $world_dir
+tar -czvf ${backup_dir}/Hellheim.tar.gz Hellheim.db Hellheim.fwl
+
+echo "##### Pushing backup to git#####"
 cd /home/vhserver/valheim_Hellheim_server
 git add .
-git commit -a -m "Adding backup Hellheim-${DateTime}.tar.gz, keeping last $BackupCount backups and deleting the rest"
+git commit -a -m "Adding backup Hellheim.tar.gz ${DateTime}, keeping last $BackupCount backups and deleting the rest"
 git push
 
+echo "##### Starting vhserver #####"
 vhserver start
 
